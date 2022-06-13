9b14a1fbbce0bac37455b80f35c5542c5602fef8
Arnd <arnd.westermann@precess.de>
2022-06-13 09:28:58 +0200

    feat(general): added test library

---
+++ libs/test/src/lib/directives/native-date-transformer/native-date-transformer.directive.spec.ts
@@ -0,0 +1,8 @@
+import { NativeDateTransformerDirective } from './native-date-transformer.directive';
+
+describe('NativeDateTransformerDirective', () => {
+  it('should create an instance', () => {
+    const directive = new NativeDateTransformerDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ libs/test/src/lib/test.module.ts
@@ -0,0 +1,7 @@
+import { NgModule } from '@angular/core';
+import { NativeDateTransformerDirective } from './directives/native-date-transformer/native-date-transformer.directive';
+
+@NgModule({
+	declarations: [NativeDateTransformerDirective],
+})
+export class TestModule {}
